d3e8f182b212876e69eed5327acd64f119b4da13
fatimah ahmad <fatima@yahoo.com>
2011-10-17 16:02:18 +0100

    the start menu done

--- lib/start.rb
+++ lib/start.rb
@@ -1,3 +1,4 @@
+  require "../lib/search"
 
  puts 'Welcome to UoM Airline'
  puts 'please choose (1) to Log In or (2) to Register '
@@ -6,13 +7,25 @@
 
  case key
    when '1'
-    puts 'Please Enter your Email Address'
+      puts 'Please Enter your Email Address'
     email=STDIN.gets.chomp
      puts 'Please Enter your PassWord'
     passWord=STDIN.gets.chomp
 
+     search(email,passWord)
+
    when '2'
-     puts 'Registration'
+      puts 'Enter your First name'
+      f_name=STDIN.gets.chomp
+      puts 'Enter your Sur name'
+      s_name=STDIN.gets.chomp
+      puts 'Enter your PassWord'
+      password=STDIN.gets.chomp
+      puts 'Enter your Email Address'
+      email=STDIN.gets.chomp
+      puts 'Enter your Address'
+      address=STDIN.gets.chomp
+    customer=Customer.new(f_name,s_name,password,email,address,"client")
      break
    else
      puts 'invalid entry, try again...'
