5f934efe4222dbb09146ed9d738c5c845087f04c
Anonymous <dannocz@hotmail.com>
2011-10-20 17:28:50 +0100

    We forgot to commit this test case

--- spec/payment_spec.rb
+++ spec/payment_spec.rb
@@ -1,6 +1,6 @@
 require "rspec"
 require "../lib/login"
-require "../lib/user_interface"
+
 require "../lib/payment"
 
 describe "My behaviour" do
@@ -9,14 +9,11 @@ describe "My behaviour" do
 
   it "should be a valid credit card number" do
     payment = Payment.new
-    payment.credit_card_no ='112123232'
+    payment.credit_card_no ='1234567887654321'
+
+    payment.validate_credit_card.should == 1
   end
 
-  it "should be correct payment amount" do
-    payment = Payment.new
-    payment.amount = '350'
-
-  end
 
   end
 
